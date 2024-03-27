0b74511a3e17f2c5a64bbab79de06e3a3ae6a0b8
Emannuel de Melo Moura <114677770+Emannuel-code@users.noreply.github.com>
2024-03-27 17:22:28 -0300

    Update Post.js

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Post.css"; // Importe o arquivo de estilos CSS
+import "./Post.css"; 
 
 function Post() {
   const { id } = useParams();
